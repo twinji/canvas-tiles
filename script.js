90831ace3f9c30d502aaf02150ee4e3d16bf7816
Vishnu Gunapathi <gunvy005@mymail.unisa.edu.au>
2021-01-26 17:14:19 +1030

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 const WIDTH = window.innerWidth, 
       HEIGHT = window.innerHeight;
 
+var tiles, tileRate;
+
+var tileSize = 12;
+
 window.onload = function(e) {
 
     // canvas setup
@@ -22,8 +26,53 @@ window.onload = function(e) {
     window.requestAnimationFrame(loop, canvas);
 }
 
-function init(c) {}
+function init(c) {
 
-function update() {}
+    var tilesX = Math.round(WIDTH / tileSize) + 1;
+    var tilesY = Math.round(HEIGHT / tileSize) + 1;
 
-function render(c) {}
+    tiles = new Array(tilesX);
+    tileRate = new Array(tilesX);
+
+    for (var x = 0; x < tiles.length; x++) {
+        tiles[x] = new Array(tilesY);
+        tileRate[x] = new Array(tilesY);
+    }
+
+    for (var x = 0; x < tiles.length; x++) {
+        for (var y = 0; y < tiles[x].length; y++) {
+            tiles[x][y] = Math.random();
+            tileRate[x][y] = Math.random() * .01;
+        }
+    }
+
+}
+
+function update() {
+
+    for (var x = 0; x < tiles.length; x++) {
+        for (var y = 0; y < tiles[x].length; y++) {
+            tiles[x][y] = (tiles[x][y] + tileRate[x][y]) % 1;
+        }
+    }
+
+}
+
+function render(c) {
+
+    c.fillStyle = "black";
+
+    for (var x = 0; x < tiles.length; x++) {
+        for (var y = 0; y < tiles[x].length; y++) {
+            c.globalAlpha = tiles[x][y];
+            c.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
+        }
+    }
+
+    // screen fade out effect
+    c.fillStyle = "crimson";
+    c.globalAlpha = .06;
+    c.fillRect(0, 0, WIDTH, HEIGHT)
+    c.globalAlpha = 1;
+
+}
